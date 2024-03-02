501b00922426253fb2acdc53a391909fcf9cfbc2
Luke Wheldale <lukewheldale@gmail.com>
2024-03-03 08:05:09 +1030

    change div color functionality into a function

--- script.js
+++ script.js
@@ -15,19 +15,17 @@ createGrid();
 
 const cells = document.querySelectorAll(".gridCell");
 
-cells.forEach((cell) => {
-	cell.addEventListener("mouseenter", () => {
-		cell.style.backgroundColor = "blue";
+function changeDivColor() {
+	cells.forEach((cell) => {
+		cell.addEventListener("mouseenter", () => {
+			cell.style.backgroundColor = "blue";
+		});
 	});
-});
-
-console.log(cells);
-
-console.log(container);
+}
+changeDivColor();
 
 // Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
 
-function changeDivColor() {}
 // Hint: “Hovering” is what happens when your mouse enters a div and ends when your mouse leaves it. You can set up event listeners for either of those events as a starting point.
 // There are multiple ways to change the color of the divs, including:
 // Adding a new class to the div.
